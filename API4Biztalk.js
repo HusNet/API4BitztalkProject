1a463083b5f0300b3dbddb529a8ef8a1b7afbda4
BuildTools <unconfigured@null.spigotmc.org>
2019-05-09 15:05:10 +0000

    small modifs

--- API4Biztalk.js
+++ API4Biztalk.js
@@ -7,24 +7,18 @@ API4Biztalk.use(express.json());        // to support JSON-encoded bodies
 
 // Server GET requests
 API4Biztalk.get('/pos/addQuota', (req, res) => function(req, res){
-    console.log(getCurrentTimestampForLogs + " : " + req.body.name);
-    res.end();
+    res.end(JSON.stringify({'responde': 'ok'}));
 });
 API4Biztalk.get('/ext/addQuota', (req, res) => function(req, res){
-    console.log(getCurrentTimestampForLogs + " : " + req.body.name);
-    res.end();
+    res.end(JSON.stringify({'responde': 'ok'}));
 });
 API4Biztalk.get('/addCardInfos', (req, res) => function(req, res){
-    console.log(getCurrentTimestampForLogs + " : " + req.body.name);
-    res.end();
+    res.end(JSON.stringify({'responde': 'ok'}));
 });
 
-// Server POST requests
-API4Biztalk.post('/monitoring/switch', (req, res) => r.requests.switchMonitoringStatus(req, res));
 
-
-API4Biztalk.listen(666, function () {
-    console.log("API4Biztalk is running on port " + 666)
+API4Biztalk.listen(6546, function () {
+    console.log("API4Biztalk is running on port " + 6546)
 });
 
 function getCurrentTimestampForLogs(){
