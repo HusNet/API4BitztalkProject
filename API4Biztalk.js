678b02c67bcf0b1c83461eb672a636cf7d623642
R-Men <huser.gaetan@hotmail.com>
2019-05-09 17:56:06 +0200

    bug fix

--- API4Biztalk.js
+++ API4Biztalk.js
@@ -6,16 +6,9 @@ const API4Biztalk = express();
 API4Biztalk.use(express.json());        // to support JSON-encoded bodies
 
 // Server GET requests
-API4Biztalk.get('/pos/addQuota', (req, res) => function(req, res){
-    res.end(JSON.stringify({'responde': 'ok'}));
-});
-API4Biztalk.get('/ext/addQuota', (req, res) => function(req, res){
-    res.end(JSON.stringify({'responde': 'ok'}));
-});
-API4Biztalk.get('/addCardInfos', (req, res) => function(req, res){
-    res.end(JSON.stringify({'responde': 'ok'}));
-});
-
+API4Biztalk.get('/pos/addQuota', (req, res) => addQuota(req, res));
+API4Biztalk.get('/ext/addQuota', (req, res) => addQuotaExt(req, res));
+API4Biztalk.get('/addCardInfos', (req, res) => addCardInfos(req, res));
 
 API4Biztalk.listen(6546, function () {
     console.log("API4Biztalk is running on port " + 6546)
@@ -23,4 +16,21 @@ API4Biztalk.listen(6546, function () {
 
 function getCurrentTimestampForLogs(){
     return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+}
+
+function addQuota(req, res){
+    printUrl(req, res);
+}
+
+function addQuotaExt(req, res){
+    printUrl(req, res);
+}
+
+function addCardInfos(req, res){
+    printUrl(req, res);
+}
+
+function printUrl(req, res){
+    console.log(getCurrentTimestampForLogs() + " : GET " + req.url + " success");
+    res.end();
 }
