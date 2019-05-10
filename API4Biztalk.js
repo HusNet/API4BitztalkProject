a0ba9996d33968e6f8cbec92b1624c2b42dbbc14
R-Men <huser.gaetan@hotmail.com>
2019-05-10 14:21:24 +0200

    improved logs

--- API4Biztalk.js
+++ API4Biztalk.js
@@ -14,17 +14,40 @@ const logger = myLoggers.getLogger("default");
 API4Biztalk.use(express.json());        // to support JSON-encoded bodies
 
 // Server GET requests
-API4Biztalk.get('/pos/addQuota', (req, res) => addQuota(req, res));
-API4Biztalk.get('/ext/addQuota', (req, res) => addQuotaExt(req, res));
-API4Biztalk.get('/addCardInfos', (req, res) => addCardInfos(req, res));
-API4Biztalk.get('/printsys/addQuota', (req, res) => addQuotaToPrintSys(req, res));
+API4Biztalk.post('/pos/addQuota', (req, res) => addQuota(req, res));
+API4Biztalk.post('/ext/addQuota', (req, res) => addQuotaExt(req, res));
+API4Biztalk.post('/addCardInfos', (req, res) => addCardInfos(req, res));
+API4Biztalk.post('/ps/addQuota', (req, res) => addQuotaToPrintSys(req, res));
 
 API4Biztalk.get('/me/want/da/logs/boay', (req, res) => {
     readLastLines.read('logs/access.log', 15)
     .then((logs) => {
         let lines = logs.split('\n');
         logger.warn("GET " + req.url + " success from " + req.ip);
-        res.end(JSON.stringify({'logs' : lines}));
+
+
+        
+
+        let output = '{ "logs": [{';
+        lines.forEach(line => {
+            if (line != '' && typeof line !== 'undefined') {
+                let l = line.split("{");
+                if (l[1] != '' && typeof l[1] !== 'undefined') {
+                    let objects = l[1].split("}"); 
+                    output += '"' + l[0] + '" : {' + objects[0] + '}';
+                
+                    output += ',';
+                }
+            }
+        });
+
+        output.substring(0, output.length-1);
+
+        output += '}]}';
+
+
+        res.setHeader('Content-Type', 'application/json');
+        res.end(output);
     });    
 });
 
@@ -33,22 +56,22 @@ API4Biztalk.listen(6546, function () {
 });
 
 function addQuota(req, res){
-    printUrl(req, res);
+    printUrl(req, res, {'uid' : req.body.uid, 'quota' : req.body.quota});
 }
 
 function addQuotaExt(req, res){
-    printUrl(req, res);
+    printUrl(req, res, {'username' : req.body.username, 'quota' : req.body.quota});
 }
 
 function addCardInfos(req, res){
-    printUrl(req, res);
+    printUrl(req, res, {'cardId' : req.body.cardId, 'uid' : req.body.uid, 'username' : req.body.username});
 }
 
 function addQuotaToPrintSys(req, res){
-    printUrl(req, res);
+    printUrl(req, res, {'username' : req.body.username, 'quota' : req.body.quota});
 }
 
-function printUrl(req, res){
-    logger.info("GET " + req.url + " success from " + req.ip);
+function printUrl(req, res, json){
+    logger.info("GET " + req.url + " success from " + req.ip + " " + JSON.stringify(json));
     res.end();
 }
